3f3eea656acf28391d3606a1ff12792fba64691f
Joshua Crum <joshua.crum@gmail.com>
2019-06-22 21:31:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,14 @@
 class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
 end
