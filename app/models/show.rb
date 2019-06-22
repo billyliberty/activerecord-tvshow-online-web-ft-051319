34a3f64eaf74b386ef501f8eb412e3408d62cda7
Joshua Crum <joshua.crum@gmail.com>
2019-06-22 21:32:46 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,6 +13,6 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    self.where("rating = ?", self.lowest_rating).first
+    self.where("rating = ?", self.highest_rating).last
   end
 end
